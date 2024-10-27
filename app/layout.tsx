97325220217416ef4cbbf7a590c3a850cecc61dd
naoya25 <onaoya2002@gmail.com>
2024-10-27 13:56:28 +0900

    feat google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3861072559853146"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         style={{ minWidth: "100vw", overflowX: "hidden", minHeight: "100vh" }}
